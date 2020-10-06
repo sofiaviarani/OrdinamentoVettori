bb673252abdd3f89dbb7d5dc544514ff99561c2e
MatteoLucchiPascal <mlucchi79@gmail.com>
2020-10-06 09:27:19 +0200

    Struttura base

---
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OrdinamentoVettori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] vettore = new int [10] { 1, 55, -9, 7, 74, 147, -87, 6, 0, 11 };
+
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(vettore[i]);
+
+            Console.ReadKey();
+        }
+
+     
+    }
+}
